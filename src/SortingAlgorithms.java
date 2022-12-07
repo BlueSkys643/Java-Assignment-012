3a978e0b90691eeeab1aed0921b67759ee0f3aaf
netlab-student <shunsinger643@gmail.com>
2022-12-07 12:15:02 -0800

    sarting via selection sort is working

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -9,11 +9,29 @@ public class SortingAlgorithms {
     read list starting at second position
     loop
     */
+    public static void SortList (int[] List) {
+        for (int i=0;i<List.length;i++) {
+            int min = findMinIndex(List,i);
+            swap(List, i, min);
+        }
+    }
 
-    /*
-    create array
-    fill array with random numbers
-    */
+    public static int findMinIndex (int[] List, int start) {
+        int smolIndex = start;
+        for (int i = start+1; i < List.length; i++) {
+            if (List[i] < List[smolIndex]) {
+                smolIndex=i;
+            }
+        }
+        return smolIndex;
+    }
+    public static void swap (int[] a, int i1, int i2) {
+        int temp = a[i1];
+        a[i1] = a[i2];
+        a[i2] = temp;
+    }
+
+    //fill array with random numbers
     public static void fillList (int[] List, int Size, int Min, int Max) {
         for (int i=0;i<Size;i++) {
             List[i] = randomNumber(Min,Max);
@@ -39,14 +57,18 @@ public class SortingAlgorithms {
         int maxNum = scan.nextInt();
         //create array using given parameters
         int[] list = new int[listSize];
-        //fill array using fillList method
+        //fill array using fillList method with given parameters
         fillList(list, listSize, minNum,maxNum);
         //print array
         for (int i=0;i<listSize;i++) {
             System.out.println(list[i]);
         }
         //sort array
-
+        SortList(list);
         //print array again
+        System.out.println("Sorted?");
+        for (int i=0;i<listSize;i++) {
+            System.out.println(list[i]);
+        }
     }
 }
